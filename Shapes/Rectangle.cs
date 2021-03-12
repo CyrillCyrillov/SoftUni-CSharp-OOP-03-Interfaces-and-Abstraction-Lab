41a5985957a8f4216a478f9181057889efb35a2e
Kiril Kirilov <kirilkirilov@abv.bg>
2021-03-12 23:44:43 +0200

    Add files via upload

---
+++ Shapes/Rectangle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : IDrawable
+    {
+        private int width;
+        private int height;
+
+        
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public int Width
+        {
+            get { return width; }
+            set { width = value; }
+        
+        }
+        public int Height
+        {
+            get { return height; }
+            set { height = value; }
+
+        }
+        
+        public void Draw()
+        {
+            Console.WriteLine("Drawing Rectangle");
+            for (int i = 0; i < Height; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Shapes/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            IDrawable figureOne = new Rectangle(6, 4);
+            figureOne.Draw();
+
+            IDrawable fugureTwo = new Circle(5);
+            fugureTwo.Draw();
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
